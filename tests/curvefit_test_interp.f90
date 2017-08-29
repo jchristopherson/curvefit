592fc70b5b206ad45ca26b545675280df2142cbc
jchristopherson <jchristopherson@hotmail.com>
2017-08-29 13:31:16 -0500

    Set up linear interpolation test

--- tests/curvefit_test_interp.f90
+++ tests/curvefit_test_interp.f90
@@ -8,11 +8,57 @@ module curvefit_test_interp
 contains
 ! ------------------------------------------------------------------------------
     function test_linear_interp() result(rst)
+        ! Parameters
+        integer(i32), parameter :: n = 100
+        integer(i32), parameter :: m = 1000
+
         ! Local Variables
         logical :: rst
+        type(linear_interp) :: interp
+        real(dp) :: dx, x(n), y(n), xi(m), yi(m)
+        integer(i32) :: i, id
 
         ! Initialization
         rst = .true.
+        dx = 0.1d0
+        x(1) = 0.0d0
+        y(1) = 0.0d0
+        do i = 2, n
+            x(i) = x(i-1) + dx
+            y(i) = exp(-0.1d0 * x(i)) * sin(5.0d1 * x(i))
+        end do
+
+        xi(1) = 0.0d0
+        dx = dx * (real(n, dp) / m)
+        do i = 2, m
+            xi(i) = xi(i-1) + dx
+        end do
+
+        ! Interpolate
+        call interp%initialize(n, 1)
+        yi = interp%interpolate(x, y, xi)
+        
+        ! Write the results to a text file so we can plot them
+        open(newunit = id, file = "linear_interp.txt", action = "write", &
+            status = "replace")
+        do i = 1, max(m, n)
+            if (m < n) then
+                if (i <= m) then
+                    write(id, '(F14.10AF14.10AF14.10AF14.10)') x(i), ",", &
+                        y(i), ",", xi(i), ",", yi(i)
+                else
+                    write(id, '(F14.10AF14.10)') x(i), ",", y(i)
+                end if
+            else
+                if (i <= n) then
+                    write(id, '(F14.10AF14.10AF14.10AF14.10)') x(i), ",", &
+                        y(i), ",", xi(i), ",", yi(i)
+                else
+                    write(id, '(AF14.10AF14.10)') ",,", xi(i), ",", yi(i)
+                end if
+            end if
+        end do
+        close(id)
     end function
 
 ! ------------------------------------------------------------------------------
