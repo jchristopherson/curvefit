6ea86ff16fb0b6c91a252f4a64db1e52672eb0b8
jchristopherson <jchristopherson@hotmail.com>
2017-09-03 14:01:54 -0500

    Create a statistics module

---
+++ src/curvefit_statistics.f90
@@ -0,0 +1,12 @@
+! curvefit_statistics.f90
+
+!> @brief \b curvefit_statistics
+!!
+!! @par Purpose
+!! To provide a set of statistical routines for exploring curve fits of sets 
+!! of numeric data.
+module curvefit_statistics
+    use curvefit_core
+    implicit none
+contains
+end module
