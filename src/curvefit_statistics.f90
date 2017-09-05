fe83ce28a41e7cd5c61898ba91cf536349fa2bfc
jchristopherson <jchristopherson@hotmail.com>
2017-09-05 09:16:05 -0500

    Add incomplete gamma function routines

--- src/curvefit_statistics.f90
+++ src/curvefit_statistics.f90
@@ -15,6 +15,8 @@ module curvefit_statistics
     public :: standard_deviation
     public :: confidence_interval
     public :: z_value
+    public :: incomplete_gamma
+    public :: incomplete_gamma_comp
 
 
 ! ******************************************************************************
@@ -47,11 +49,17 @@ module curvefit_statistics
     end interface
 
 ! ------------------------------------------------------------------------------
-    !> @brief Computes the z-score given a percentage of the area under the
-    !! standard normal distribution curve.
+    !> @brief Computes the z-value (z-score) given a percentage of the area 
+    !! under the standard normal distribution curve.
     interface z_value
         module procedure :: std_norm_dist_z_score
     end interface
+
+! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
 
 contains
 ! ******************************************************************************
@@ -222,12 +230,18 @@ contains
     end function
 
 ! ------------------------------------------------------------------------------
-    !> @brief Computes the z-score given a percentage of the area under the
-    !! standard normal distribution curve.
+    !> @brief Computes the z-value (z-score) given a percentage of the area 
+    !! under the standard normal distribution curve.
     !!
     !! @param[in] alpha The percentage of the area under the curve.  This value
     !!  must be between 0 and 1 such that: 0 < alpha < 1.
-    !! @param[out] err
+    !! @param[out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - CF_INVALID_INPUT_ERROR: Occurs if @p alpha is does not satisfy:
+    !!      0 < alpha < 1.
     !!
     !! @return The z-score or z-value by solving for z where: 
     !!  alpha = ERF(z / sqrt(2)), where ERF is the error function.
@@ -243,6 +257,7 @@ contains
 
         ! Parameters
         real(dp), parameter :: zero = 0.0d0
+        real(dp), parameter :: one = 1.0d0
         real(dp), parameter :: two = 2.0d0
         real(dp), parameter :: ten = 1.0d1
 
@@ -251,13 +266,29 @@ contains
         procedure(fcn1var), pointer :: fcn
         type(brent_solver) :: solver
         type(value_pair) :: lim
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+
+        ! Initialization
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+
+        ! Input Check
+        if (alpha <= zero .or. alpha >= one) then
+            call errmgr%report_error("std_norm_dist_z_score", &
+                "The alpha parameter must lie between 0 and 1.", &
+                CF_INVALID_INPUT_ERROR)
+        end if
 
         ! Compute the solution
         fcn => zfun
         call obj%set_fcn(fcn)
         lim%x1 = zero
         lim%x2 = ten
-        call solver%solve(obj, z, lim, err = err)
+        call solver%solve(obj, z, lim, err = errmgr)
 
     contains
         ! Compute the solution to: alpha = erf(z / sqrt(2)) for z
@@ -271,6 +302,241 @@ contains
 ! ------------------------------------------------------------------------------
 
 ! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+
+! ------------------------------------------------------------------------------
+
+! ******************************************************************************
+! GAMMA FUNCTION ROUTINES
+! ------------------------------------------------------------------------------
+    !> @brief Computes the incomplete gamma function: 
+    !! P(a,x) = 1 / gamma(a) * integrate(exp(-t) * t**(a - 1), t, 0, x).
+    !!
+    !! @param[in] a The coefficient.  This parameter must be positive-valued.
+    !! @param[in] x The independent variable.  This parameter must be greater
+    !!  than or equal to zero.
+    !! @param[out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - CF_INVALID_INPUT_ERROR: Occurs if @p x is negative, or if @p a is not
+    !!      positive.
+    !!
+    !! @param return The value of the function at @p x.
+    !!
+    !! @par Remarks
+    !! This implementation is based upon the Numerical Recipes implementation
+    !! found in section 6.2 of the text (routine: gammp).
+    function incomplete_gamma(a, x, err) result(g)
+        ! Arguments
+        real(dp), intent(in) :: a, x
+        class(errors), intent(inout), optional, target :: err
+        real(dp) :: g
+
+        ! Parameters
+        real(dp), parameter :: zero = 0.0d0
+        real(dp), parameter :: one = 1.0d0
+
+        ! Local Variables
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+
+        ! Initialization
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+        g = zero
+
+        ! Input Check
+        if (x < zero) then
+            call errmgr%report_error("incomplete_gamma", &
+                "The independent variable (x) must be >= 0.", &
+                CF_INVALID_INPUT_ERROR)
+            return
+        else if (a <= zero) then
+            call errmgr%report_error("incomplete_gamma", &
+                "The parameter a must be positive.", CF_INVALID_INPUT_ERROR)
+            return
+        end if
+
+        ! Process
+        if (x < a + one) then
+            g = inc_gamma_series(a, x)
+        else
+            g = one - inc_gamma_cf(a, x)
+        end if
+    end function
+
+! ------------------------------------------------------------------------------
+    !> @brief Computes the complement of the incomplete gamma function: 
+    !! Q(a,x) = 1 - P(a,x), where 
+    !! P(a,x) = 1 / gamma(a) * integrate(exp(-t) * t**(a - 1), t, 0, x).
+    !!
+    !! @param[in] a The coefficient.  This parameter must be positive-valued.
+    !! @param[in] x The independent variable.  This parameter must be greater
+    !!  than or equal to zero.
+    !! @param[out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - CF_INVALID_INPUT_ERROR: Occurs if @p x is negative, or if @p a is not
+    !!      positive.
+    !!
+    !! @param return The value of the function at @p x.
+    !!
+    !! @par Remarks
+    !! This implementation is based upon the Numerical Recipes implementation
+    !! found in section 6.2 of the text (routine: gammq).
+    function incomplete_gamma_comp(a, x, err) result(g)
+        ! Arguments
+        real(dp), intent(in) :: a, x
+        class(errors), intent(inout), optional, target :: err
+        real(dp) :: g
+
+        ! Parameters
+        real(dp), parameter :: zero = 0.0d0
+        real(dp), parameter :: one = 1.0d0
+
+        ! Local Variables
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+
+        ! Initialization
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+        g = zero
+
+        ! Input Check
+        if (x < zero) then
+            call errmgr%report_error("incomplete_gamma_comp", &
+                "The independent variable (x) must be >= 0.", &
+                CF_INVALID_INPUT_ERROR)
+            return
+        else if (a <= zero) then
+            call errmgr%report_error("incomplete_gamma_comp", &
+                "The parameter a must be positive.", CF_INVALID_INPUT_ERROR)
+            return
+        end if
+
+        ! Process
+        if (x < a + one) then
+            g = one - inc_gamma_series(a, x)
+        else
+            g = inc_gamma_cf(a, x)
+        end if
+    end function
+
+! ------------------------------------------------------------------------------
+    !> @brief Computes the incomplete gamma function: 
+    !! P(a,x) = 1 / gamma(a) * integrate(exp(-t) * t**(a - 1), t, 0, x)
+    !! is computed by its series representation.
+    !!
+    !! @param[in] a The parameter a.
+    !! @param[in] x The parameter x.  This parameter must be greater than 0.
+    !!
+    !! @return The incomplete gamma function.
+    !!
+    !! @par Remarks
+    !! This implementation is based upon the Numerical Recipes implementation
+    !! found in section 6.2 of the text (routine: gser).
+    pure function inc_gamma_series(a, x) result(g)
+        ! Arguments
+        real(dp), intent(in) :: a, x
+        real(dp) :: g
+
+        ! Parameters
+        integer(i32), parameter :: itmax = 500
+        real(dp), parameter :: zero = 0.0d0
+        real(dp), parameter :: one = 1.0d0
+
+        ! Local Variables
+        integer(i32) :: n
+        real(dp) :: ap, del, sm, gln, eps
+
+        ! Initialization
+        eps = epsilon(eps)
+        gln = log_gamma(a)
+        ap = a
+        sm = one / a
+        del = sm
+        g = zero
+        do n = 1, itmax
+            ap = ap + one
+            del = del * x / ap
+            sm = sm + del
+            if (abs(del) < abs(sm) * eps) then
+                g = sm * exp(-x + a * log(x) - gln)
+                return
+            end if
+        end do
+
+        ! If we're here, it didn't converge
+    end function
+
+! ------------------------------------------------------------------------------
+    !> @brief Computes the incomplete gamma function: 
+    !! Q(a,x) = 1 - P(a,x), where 
+    !! P(a,x) = 1 / gamma(a) * integrate(exp(-t) * t**(a - 1), t, 0, x)
+    !! is computed by Lentz's continued fraction approach.
+    !!
+    !! @param[in] a The parameter a.
+    !! @param[in] x The parameter x.  This parameter must be greater than 0.
+    !!
+    !! @return The incomplete gamma function.
+    !!
+    !! @par Remarks
+    !! This implementation is based upon the Numerical Recipes implementation
+    !! found in section 6.2 of the text (routine: gcf).
+    pure function inc_gamma_cf(a, x) result(g)
+        ! Arguments
+        real(dp), intent(in) :: a, x
+        real(dp) :: g
+
+        ! Parameters
+        integer(i32), parameter :: itmax = 500
+        real(dp), parameter :: zero = 0.0d0
+        real(dp), parameter :: one = 1.0d0
+        real(dp), parameter :: two = 2.0d0
+
+        ! Local Variables
+        integer(i32) :: i
+        real(dp) :: an, b, c, d, del, h, gln, eps, fpmin
+
+        ! Initialization
+        eps = epsilon(eps)
+        fpmin = tiny(fpmin) / eps
+        gln = log_gamma(a)
+        b = x + one - a
+        c = one / fpmin
+        d = one / b
+        h = d
+        g = zero
+        do i = 1, itmax
+            an = -i * (i - a)
+            b = b + two
+            d = an * d + b
+            if (abs(d) < fpmin) d = fpmin
+            c = b + an / c
+            if (abs(c) < fpmin) c = fpmin
+            d = one / d
+            del = d * c
+            h = h * del
+            if (abs(del - one) < eps) then
+                g = exp(-x + a * log(x) - gln) * h
+                return
+            end if
+        end do
+
+        ! If we're here, it didn't converge
+    end function
 
 ! ------------------------------------------------------------------------------
 
