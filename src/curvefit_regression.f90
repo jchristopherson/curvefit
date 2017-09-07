39223c0d5e1768e592d72b77eb06cfbc441ce433
jchristopherson <jchristopherson@hotmail.com>
2017-09-07 07:52:32 -0500

    Establish a curvefit_regression module

---
+++ src/curvefit_regression.f90
@@ -0,0 +1,9 @@
+! curvefit_regression.f90
+
+!> @brief \b curvefit_regression
+!!
+!! @par Purpose
+!! To provide routines for perforing regression operations on sets of numerical
+!! data.
+module curvefit_regression
+end module
