a64f862d50f276677d6d309960d1bd0361ea2ad1
jchristopherson <jchristopherson@hotmail.com>
2017-09-27 08:02:34 -0500

    Add the curvefit_calibration module

---
+++ src/curvefit_calibration.f90
@@ -0,0 +1,9 @@
+! curvefit_calibration.f90
+
+!> @brief \b curvefit_calibration
+!!
+!! @par Purpose
+!! To provide routines for computing calibration performance metrics commonly 
+!! used to assess the fitness of a calibration curve fit.
+module curvefit_calibration
+end module
