5207e5638e13eec78f5766605a66b03a8144934b
Jason Christopherson <jchristopherson@hotmail.com>
2018-05-25 14:24:43 -0500

    Add a nonlinear regression example

--- src/curvefit_regression.f90
+++ src/curvefit_regression.f90
@@ -248,6 +248,98 @@ module curvefit_regression
 
 ! ------------------------------------------------------------------------------
     !> @brief A type for supporting nonlinear regression calculations.
+    !!
+    !! @par Example
+    !! The following example illustrates the use of the nonlinear_regression
+    !! type to fit an equation to a noisy set of data.
+    !! @code{.f90}
+    !! program example
+    !!     use iso_fortran_env
+    !!     use curvefit_regression
+    !!     use curvefit_core
+    !!     use fplot_core
+    !!     implicit none
+    !!
+    !!     ! Parameters
+    !!     integer(int32), parameter :: npts = 100
+    !!     real(real64), parameter :: xMin = 0.0d0
+    !!     real(real64), parameter :: xMax = 1.0d0
+    !!     real(real64), parameter :: a = 2.0d0
+    !!     real(real64), parameter :: w = 30.0d0
+    !!     real(real64), parameter :: d = 0.2d0
+    !!     real(real64), parameter :: p = 0.0d0
+    !!
+    !!     ! Local variables
+    !!     procedure(reg_fcn), pointer :: fun
+    !!     real(real64) :: x(npts), y(npts), c(4), yfit(npts)
+    !!     type(nonlinear_regression) :: solver
+    !!     type(plot_2d) :: plt
+    !!     type(plot_data_2d) :: d1, d2
+    !!     integer(int32) :: i
+    !!
+    !!     ! Define the data to fit
+    !!     x = linspace(xMin, xMax, npts)
+    !!     call random_number(y)
+    !!     y = a * exp(-w * d * x) * sin(w * x - p) + (y - 0.5d0)
+    !!
+    !!     ! Set up the solver.  Assume an equation of the form:
+    !!     !   A exp(-w d x) sin(w * x - p)
+    !!     fun => fcn
+    !!     c = [1.0d0, 20.0d0, 0.0d0, 0.0d0]
+    !!     call solver%initialize(x, y, fun, size(c))
+    !!
+    !!     ! Solve
+    !!     call solver%solve(c)
+    !!
+    !!     ! Compute the fitted form of the routine
+    !!     do i = 1, size(x)
+    !!         yfit(i) = fun(x(i), c)
+    !!     end do
+    !!
+    !!     ! Print out the coefficients
+    !!     print '(AF8.4AF8.4)', "A = ", c(1), ", Actual = ", a
+    !!     print '(AF8.4AF8.4)', "w = ", c(2), ", Actual = ", w
+    !!     print '(AF8.4AF8.4)', "d = ", c(3), ", Actual = ", d
+    !!     print '(AF8.4AF8.4)', "p = ", c(4), ", Actual = ", p
+    !!
+    !!     ! Plot the results
+    !!     call plt%initialize()
+    !!     call plt%set_font_size(14)
+    !!
+    !!     call d1%set_name("Raw Data")
+    !!     call d1%set_line_color(CLR_BLACK)
+    !!     call d1%set_draw_line(.false.)
+    !!     call d1%set_draw_markers(.true.)
+    !!     call d1%set_marker_style(MARKER_X)
+    !!     call d1%define_data(x, y)
+    !!
+    !!     call d2%set_name("Fit")
+    !!     call d2%set_line_color(CLR_BLUE)
+    !!     call d2%set_line_width(2.0)
+    !!     call d2%define_data(x, yfit)
+    !!
+    !!     call plt%push(d1)
+    !!     call plt%push(d2)
+    !!     call plt%draw()
+    !!
+    !! contains
+    !!     function fcn(x, c) result(f)
+    !!         real(real64), intent(in) :: x
+    !!         real(real64), intent(in), dimension(:) :: c
+    !!         real(real64) :: f
+    !!
+    !!         f = c(1) * exp(-c(2) * c(3) * x) * sin(c(2) * x - c(4))
+    !!     end function
+    !! end program
+    !! @endcode
+    !! The above program produces the following outputs.
+    !! @code{.txt}
+    !! A =   1.9637, Actual =   2.0000
+    !! w =  30.6889, Actual =  30.0000
+    !! d =   0.1947, Actual =   0.2000
+    !! p =   0.1400, Actual =   0.0000
+    !! @endcode
+    !! @image html nlreg_example_2.png
     type, extends(vecfcn_helper) :: nonlinear_regression
         private
         !> A pointer to the routine containing the function of interest.
@@ -1539,13 +1631,13 @@ contains
     !!     ! could have been avoided.
     !!     gain = linear_least_squares(transpose(bridge), transpose(loads))
     !!
-    !!     ! Apply the matrix
+    !!     ! Apply the matrix.
     !!     calib = matmul(bridge, transpose(gain))
     !!
     !!     ! Compute errors in the fit
     !!     errs = calib - loads
     !!
-    !!     ! Express the loads in terms of percent of full scale
+    !!     ! Express the errors in terms of percent of full scale
     !!     axialFullScale = maxval(loads(:,1))
     !!     torqueFullScale = maxval(loads(:,2))
     !!     errs(:,1) = 1.0d2 * errs(:,1) / axialFullScale
